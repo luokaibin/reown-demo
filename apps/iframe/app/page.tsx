310a2c7342800d2c87d9c428bb20d5d81f793fa3
colinluo <colin.luo@kikitrade.com>
2024-12-19 10:52:14 +0800

    feat: iframe

--- apps/iframe/app/page.tsx
+++ apps/iframe/app/page.tsx
@@ -2,14 +2,16 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  const onClick = () => {
+  const requestStorageAccess = async () => {
+    const storage = await document.requestStorageAccess({all: true})
+    console.log('line 7', storage);
     localStorage.setItem('test', 'test')
   }
   return (
     <div className={styles.page}>
       <h1>Iframe</h1>
-      <div onClick={onClick}>
-        添加值
+      <div className={styles.action}>
+        <button onClick={requestStorageAccess}>requestStorageAccess</button>
       </div>
       <appkit-button />
     </div>
