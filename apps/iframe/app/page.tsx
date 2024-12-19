34914f3db596c2700833ce1298436dc5d6e4ff1c
colinluo <colin.luo@kikitrade.com>
2024-12-19 11:08:17 +0800

    feat: iframe

--- apps/iframe/app/page.tsx
+++ apps/iframe/app/page.tsx
@@ -3,10 +3,17 @@ import styles from "./page.module.css";
 
 export default function Home() {
   const requestStorageAccess = async () => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-    const storage = await document.requestStorageAccess({all: true})
-    console.log('line 7', storage);
+    const hasAccess = await document.hasStorageAccess();
+    if (!hasAccess) {
+      // 需要请求权限
+      try {
+        await document.requestStorageAccess();
+        console.log(localStorage, localStorage.getItem('test'))
+        console.log('存储访问权限已授予');
+      } catch (error) {
+        console.error('无法获取存储访问权限:', error);
+      }
+    }
     localStorage.setItem('test', 'test')
   }
   return (
